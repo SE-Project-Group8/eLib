2eb0fa831146359eb8c8d8dd148949ddae27e3a9
Shivam Patel <shivampatelzatchbell234@gmail.com>
2021-10-09 20:58:59 +0530

    First Commit

---
+++ models/issue.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const issueSchema = new mongoose.Schema({
+   book_info : {
+       id : {
+           type : mongoose.Schema.Types.ObjectId,
+           ref : 'Book', 
+       },
+       title : String,
+       author : String,
+       ISBN : String,
+       category : String,
+       stock : Number,
+       price: Number,
+       issueDate : {type : Date, default : Date.now()},
+       returnDate : {type : Date, default : Date.now() + 10*24*60*60*1000},
+       isRenewed : {type : Boolean, default : false},
+   }, 
+   
+   user_id : {
+       id : {
+           type : mongoose.Schema.Types.ObjectId,
+           ref : 'User',
+       },
+       
+       username : String,
+   },
+});
+
+
+module.exports = mongoose.model("Issue", issueSchema);
